63097971145e1d556cbd236a35e34728a972c225
bk0106 <chris.h@miasfashion.com>
2024-01-22 11:05:41 -0800

    Added Regist

--- MiasHRSolution/MiasHR.Web/Services/Contracts/IAuthService.cs
+++ MiasHRSolution/MiasHR.Web/Services/Contracts/IAuthService.cs
@@ -6,7 +6,7 @@ namespace MiasHR.Web.Services.Contracts
     public interface IAuthService
     {
         Task<HttpResponseMessage> Login(UserDTO request);
-        Task<RequestResultDTO> Register(UserDTO request, DateOnly birthDate);
+        Task<HttpResponseMessage> Register(UserDTO request, DateOnly birthDate);
         Task Logout();
         Task<bool> IsLoggedIn();
         Task<string?> GetUserEmplCode();
