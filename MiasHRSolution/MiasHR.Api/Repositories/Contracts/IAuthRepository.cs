c14812fb4204f887620f740c65a77166f471e8a2
bk0106 <chris.h@miasfashion.com>
2024-03-12 13:31:31 -0700

    Change name to GetUserEmail

--- MiasHRSolution/MiasHR.Api/Repositories/Contracts/IAuthRepository.cs
+++ MiasHRSolution/MiasHR.Api/Repositories/Contracts/IAuthRepository.cs
@@ -9,6 +9,6 @@ namespace MiasHR.Api.Repositories.Contracts
         Task<Tuple<HrEmployee, bool>>? Login(string username, string passwordHash);
         Task<RequestResultDTO> UpdateUserPassword(string emplCode, string newPasswordHash);
         Task<UpdateMessageDTO> GetUserExist(string username, DateOnly birthDate);
-        Task<UpdateMessageDTO> GetEmail(string emplCode);
+        Task<UpdateMessageDTO> GetUserEmail(string emplCode);
     }
 }
