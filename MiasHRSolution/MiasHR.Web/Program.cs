c8e0543f2b594950d319221b9ac110301cf7eb25
Jc@mias41060 <jaehoon.c@miasfashion.com>
2024-03-29 15:27:27 -0700

    remove code for testing

--- MiasHRSolution/MiasHR.Web/Program.cs
+++ MiasHRSolution/MiasHR.Web/Program.cs
@@ -14,7 +14,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-
 if (builder.HostEnvironment.Environment == "Production")
 {
     builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.miasfashion.com:8080/") });
