f286d94bb857850c38c87e6d62cace1d7d4ac036
bk0106 <chris.h@miasfashion.com>
2024-07-15 15:41:34 -0700

    Component of Employee Time Off Info

--- MiasHRSolution/MiasHR.Web/Services/Contracts/IEmployeeInfoRequestService.cs
+++ MiasHRSolution/MiasHR.Web/Services/Contracts/IEmployeeInfoRequestService.cs
@@ -1,9 +1,14 @@
 ﻿using MiasHR.Models.DTOs;
+using MiasHR.Web.Shared;
 
 namespace MiasHR.Web.Services.Contracts
 {
     public interface IEmployeeInfoRequestService
     {
         Task<IReadOnlyList<ManagerEmployeeListDTO>> GetManagerEmployeeList(string managerEmplCode);
+        List<ModelData> GetDefaultVacData();
+        List<ModelData> GetDefaultVacTable();
+        List<ModelData> GetDefaultSickData();
+        List<ModelData> GetDefaultSickTable();
     }
 }
--- MiasHRSolution/MiasHR.Web/Services/EmployeeInfoRequestService.cs
+++ MiasHRSolution/MiasHR.Web/Services/EmployeeInfoRequestService.cs
@@ -1,5 +1,6 @@
 ﻿using MiasHR.Models.DTOs;
 using MiasHR.Web.Services.Contracts;
+using MiasHR.Web.Shared;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -46,5 +47,47 @@ namespace MiasHR.Web.Services
                 throw new Exception("An error occurred while retrieving manager employee list.", ex);
             }
         }
+
+        public List<ModelData> GetDefaultVacData()
+        {
+            return new List<ModelData>
+        {
+            new ModelData { Group = " ", Category = "Used", Value = 0, Color = "#616161" },
+            new ModelData { Group = " ", Category = "Available", Value = 0, Color = "#1DE9B6" },
+            new ModelData { Group = " ", Category = "Available Later", Value = 0, Color = "#B0BEC5" }
+        };
+        }
+
+        public List<ModelData> GetDefaultVacTable()
+        {
+            return new List<ModelData>
+        {
+            new ModelData { Group = " ", Category = "Carried Over", Value = 0 },
+            new ModelData { Group = " ", Category = "Accrued", Value = 0 },
+            new ModelData { Group = " ", Category = "Adjusted", Value = 0 },
+            new ModelData { Group = " ", Category = "Used", Value = 0 },
+            new ModelData { Group = "Total", Category = "Available", Value = 0 }
+        };
+        }
+
+        public List<ModelData> GetDefaultSickData()
+        {
+            return new List<ModelData>
+        {
+            new ModelData { Category = "Used", Value = 0, Color = "#616161" },
+            new ModelData { Category = "Available", Value = 0, Color = "#1DE9B6" }
+        };
+        }
+
+        public List<ModelData> GetDefaultSickTable()
+        {
+            return new List<ModelData>
+        {
+            new ModelData { Category = "Sick Days", Value = 5, Color = "#616161" },
+            new ModelData { Category = "Used", Value = 0, Color = "#616161" },
+            new ModelData { Group = "Total", Category = "Available", Value = 0, Color = "#1DE9B6" }
+        };
+        }
+
     }
 }
